f5ba60d4dc5a214bd5bf5199205d598e6f69841b
Jucelio Delmondes <jucelio.2013@hotmail.com>
2020-09-09 02:29:28 -0300

    Reducao linhas

--- process.js
+++ process.js
@@ -1,6 +1,6 @@
 module.exports = {
     validate(code){ //code será a linha digitável
-        code = this.formatarCodigo(code);
+        code = code.replace(/\D/g, '')
         //Regra de negócio para a validação
         //Variáveis inicializadas com mensagem de erro para evitar um código extenso. Em caso de sucesso, será alterada 
         let obj = {
@@ -20,38 +20,13 @@ module.exports = {
             const dvCampo2 = parseInt(code.slice(20, 21));
             const campo3 = code.slice(21, 31);
             const dvCampo3 = parseInt(code.slice(31, 32));
-            const campos = (campo1+campo2+campo3).split("").map(Number); //Numeros dos campos sem o D.V de cada um. Necessário para calcular e validar se é gual ao d.v retirado acima
             const fatorVencimento = parseInt(code.slice(33, 37));
-            //passo A (arquivo titulo.pdf) Multiplicando a sequência dos campos pelos multiplicadores, iniciando por 2 da direita para a esquerda:
-            let resultadoMultiplicacao = this.multiplicacaoDv(campos);
-            //Passo B (arquivo titulo.pdf) Some, individualmente, os algarismos dos resultados do produtos:
-            //========================================================
-            let campo1Somado = 0, campo2Somado = 0, campo3Somado = 0;
-            resultadoMultiplicacao.slice(0, 9).map(result => campo1Somado+=result);
-            resultadoMultiplicacao.slice(9, 19).map(result => campo2Somado+=result);
-            resultadoMultiplicacao.slice(19, 29).map(result => campo3Somado+=result);
-            //Passo C Divida o total encontrado por 10, a fim de determinar o resto da divisão:
-            //==================================
-            const restoCampo1 = campo1Somado % 10 
-            const restoCampo2 = campo2Somado % 10
-            const restoCampo3 = campo3Somado % 10
-            //Passo D Subtrair o “resto” apurado pela dezena imediatamente posterior. O resultado será igual ao DV
-            //=========================            
-            let dezenaPosteriorCampo1 = parseInt(campo1Somado.toString().slice(0,1)+"0")+10; //captura o primeiro algarismo e concatena com 0, converte para int esoma mais 10
-            let dezenaPosteriorCampo2 = parseInt(campo2Somado.toString().slice(0,1)+"0")+10;
-            let dezenaPosteriorCampo3 = parseInt(campo3Somado.toString().slice(0,1)+"0")+10;
-            //Substraindo dezena posterior com resto e capturando segundo algarismo (D.V)
-            let dvCampo1Calculo = parseInt(parseInt(dezenaPosteriorCampo1-restoCampo1).toString().slice(1,2));
-            let dvCampo2Calculo = parseInt(parseInt(dezenaPosteriorCampo2-restoCampo2).toString().slice(1,2));
-            let dvCampo3Calculo = parseInt(parseInt(dezenaPosteriorCampo3-restoCampo3).toString().slice(1,2));
-            if(dvCampo1Calculo === dvCampo1 && dvCampo2Calculo === dvCampo2 && dvCampo3Calculo === dvCampo3) { //se os quatros digitos verificadors estiverem válidos
+            if(this.validarDVCampo(campo1, dvCampo1) && this.validarDVCampo(campo2, dvCampo2) && this.validarDVCampo(campo3, dvCampo3)){ //se os três digitos estiverem válidos            
                 let codigoBarra = code.slice(0, 4)+code.slice(32, 47)+code.slice(4, 9)+code.slice(10, 20)+code.slice(21, 31);
                 if(this.validarDVGeral(codigoBarra)) { //Valida o digito geral do codigo de barras, parâmetro 1 (tipo boleto bancário)
-                    obj.mensagem = "Boleto válido";
+                    obj.mensagem = "Boleto válido"; 
                     obj.valido = true;
-                    obj.codigoBarra = codigoBarra;
-                     //Calculando valor do boleto
-                    //Divide por 100 para separar os centavos                 
+                    obj.codigoBarra = codigoBarra;                
                     obj.valor = (code.slice(37, 47)/100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }).replace(/\,/g, ".").replace(/\.+\d{2}$/g, ",")+code.slice(45, 47);
                     //Cálculo de vencimento
                     if(fatorVencimento !== 0000) {
@@ -83,10 +58,10 @@ module.exports = {
             let resultadoMultiplicacaoCampo4 = this.multiplicacaoDv(campo4);
             //soma todos os algarismos do resultado da multiplicação
             let campo1Somado = 0, campo2Somado = 0, campo3Somado = 0, campo4Somado = 0;
-            resultadoMultiplicacaoCampo1.map(result => campo1Somado+=result);
-            resultadoMultiplicacaoCampo2.map(result => campo2Somado+=result);
-            resultadoMultiplicacaoCampo3.map(result => campo3Somado+=result);
-            resultadoMultiplicacaoCampo4.map(result => campo4Somado+=result);
+            resultadoMultiplicacaoCampo1.forEach(result => campo1Somado+=result);
+            resultadoMultiplicacaoCampo2.forEach(result => campo2Somado+=result);
+            resultadoMultiplicacaoCampo3.forEach(result => campo3Somado+=result);
+            resultadoMultiplicacaoCampo4.forEach(result => campo4Somado+=result);
             //captura o resto da divisao para depois subtrair com 10
             let restoDVCampo1 = parseInt(campo1Somado % 10)
             let restoDVCampo2 = parseInt(campo2Somado % 10)
@@ -103,7 +78,9 @@ module.exports = {
                 //SV = Segunda validação
                 let campo1SV = code.slice(0,3); //SV = campo 1 de segunda validação
                 let dv = parseInt(code.slice(3, 4)); //digito verificador
-                let campo2SV = code.slice(4, 11)+campo2+campo3+campo4 //campo restante sem o DV 
+                let campo2SV = code.slice(4, 11)+campo2+campo3+campo4 //campo restante sem o DV
+                let campos = (campo1SV+campo2SV).split("").map(Number);
+                console.log("a" +campos);
                 let resultadoMultiplicacaoSV = this.multiplicacaoDv(campo1SV+campo2SV);
                 let campoSomadoSV = 0;
                 resultadoMultiplicacaoSV.map(result => {campoSomadoSV += result})
@@ -121,22 +98,32 @@ module.exports = {
         }
         return obj;
     },
+    validarDVCampo(campo, dv) {
+        //retorna se o dv é valido com a sequencia numérica do campo
+        campo = campo.split('').map(Number);
+        let mult = this.multiplicacaoDv(campo);
+        let soma = 0;
+        //soma todos os algarismos da multiplicacao
+        mult.slice(0, mult.length).forEach(result => soma+=result);
+        //Passo C Divida o total encontrado por 10, a fim de determinar o resto da divisão:
+        const resto = parseInt(soma % 10);
+        //Passo D Subtrair o “resto” apurado pela dezena imediatamente posterior. O resultado será igual ao DV
+        let dezenaPosterior = parseInt(soma.toString().slice(0,1)+"0")+10; //captura o primeiro algarismo e concatena com 0, converte para int esoma mais 10
+        //Substraindo dezena posterior com resto e capturando segundo algarismo (D.V)
+        const dvFinal = parseInt((dezenaPosterior-resto).toString().slice(1,2))
+        return dvFinal === dv;
+    },
+    //realiza a multiplicacao da direita para a esquerda na ordem 2,1,2,1,2,1....
     multiplicacaoDv(sequencia) {
-        let resultadoMultiplicacao = [];
-        for(i = sequencia.length-1; i >= 0; i--) { //percorre os numeros dos campos sem o D.V para multiplicar
-            if(i % 2 == 0) {
-                /*Verifica se o I é par ou não, este algoritmo será para aplicar a divisao na ordem (divide por 2 e depois por 1)*/
-                let temp = sequencia[i]*2; //multiplica por 2
-                if(temp > 9) {
-                    //somar os dois algarismos
-                    let algarismo1 = parseInt(temp.toString().slice(0, 1));
-                    let algarismo2 = parseInt(temp.toString().slice(1, 2));
-                    temp = algarismo1+algarismo2;
-                }
-                resultadoMultiplicacao.unshift(parseInt(temp)); //adiciona no começo da array
-            } else resultadoMultiplicacao.unshift(sequencia[i]*1); //adiciona sempre no começo da array
-        }
-        return resultadoMultiplicacao;
+        console.log(sequencia);
+        return sequencia.reverse().map((seq, i) => {
+            if (i % 2 === 0 ){ 
+                let temp = seq * 2
+                if (temp > 9) {
+                    return Number(temp.toString()[0]) + Number(temp.toString()[1])
+                } else return temp
+            } else return seq
+        }).reverse(); 
     },
     validarDVGeral(codigoBarraCompleto) {
         //Valida o quinto dígito do codigo de barra com o cálculo
@@ -153,19 +140,5 @@ module.exports = {
         let resultadoDV = parseInt(11 - resto)
         if(resultadoDV === 0 || resultadoDV === 10 || resultadoDV === 11) resultadoDV = 1; //REGRA I) da página 14 - PDF Titulo.pdf
         if(codigoDVGeral === resultadoDV) return true; else return false;
-    },
-    formatarCodigo(linhaDigitavel) {
-        //Formata a linha digitável e retorna
-        const caracteresPermitidos = ['0','1','2','3','4','5','6','7','8','9'];
-        linhaDigitavel = linhaDigitavel.split('');
-        let novoCodigo = "";
-        linhaDigitavel.map(code => {
-            let caractereProibido = true;
-            caracteresPermitidos.map(char => {
-                if(code === char) caractereProibido = false; //se conter na array acima, o caractere atual nao é proibido
-            })
-            if(caractereProibido === false) novoCodigo += code;
-        })
-        return novoCodigo;
     }
 }
